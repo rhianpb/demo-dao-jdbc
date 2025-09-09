bd449c8b60ed0248f64894225dba8382537c64fe
rhianpb <rhian.pablo16@gmail.com>
2025-09-09 17:03:40 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.sql.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -12,6 +13,8 @@ public class Program {
 
 	public static void main(String[] args) {
 		//MySQLConnector = Projeto que utiliza o MySQL para acesso de dados em um banco de dados relacional
+		
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();//criação de um sellerDao usando uma fabrica.
 		
@@ -36,7 +39,21 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(0), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
-
+		
+		System.out.println("\n=== Test 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update complete");
+		
+		System.out.println("\n=== Test 6: seller delete ====");
+		System.out.println("Enter id for delete test: ");
+		int id = sc.nextInt();
+		sellerDao.deleteById(id);
+		System.out.println("Delete completed");
+		
+		sc.close();
+		
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -71,8 +71,8 @@ public class SellerDaoJDBC implements SellerDao{
 		try {
 			st = conn.prepareStatement(
 				    "UPDATE seller "
-				    + "SET Name = ?, SET Email = ?, SET BirthDate = ?, SET BaseSalary = ?, SET DepartmentId = ?) "
-				    + "WHERE Id = ?");
+				  + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+				  + "WHERE Id = ?");
 
 			
 			st.setString(1, obj.getName());
@@ -95,36 +95,60 @@ public class SellerDaoJDBC implements SellerDao{
 	}
 
 	@Override
-	public void deleteById(Seller id) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Seller findById(Integer id) {
+	public void deleteById(Integer id) {
 		PreparedStatement st = null;
-		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE seller.Id = ?");
+			st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+			
 			st.setInt(1, id);
-			rs = st.executeQuery();
-			if(rs.next()) {
-				Department dep = instantiateDepartment(rs);
-				Seller obj = instantiateSeller(rs, dep);
-			}
-			return null;
+			
+			st.executeUpdate();
 		}
 		catch(SQLException e) {
 			throw new DbException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
-			DB.closeResultSet(rs);
 		}
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+	    PreparedStatement st = null;
+	    ResultSet rs = null;
+	    try {
+	        st = conn.prepareStatement(
+	            "SELECT seller.*, department.Name as DepName "
+	          + "FROM seller INNER JOIN department "
+	          + "ON seller.DepartmentId = department.Id "
+	          + "WHERE seller.Id = ?");
+	        
+	        st.setInt(1, id);
+	        rs = st.executeQuery();
+	        
+	        if (rs.next()) {
+	            Department dep = new Department();
+	            dep.setId(rs.getInt("DepartmentId"));
+	            dep.setName(rs.getString("DepName"));
+
+	            Seller obj = new Seller();
+	            obj.setId(rs.getInt("Id"));
+	            obj.setName(rs.getString("Name"));
+	            obj.setEmail(rs.getString("Email"));
+	            obj.setBaseSalary(rs.getDouble("BaseSalary"));
+	            obj.setBirthDate(rs.getDate("BirthDate"));
+	            obj.setDepartment(dep);
+	            return obj;
+	        }
+	        return null; // se não encontrar
+	    }
+	    catch (SQLException e) {
+	        throw new DbException(e.getMessage());
+	    }
+	    finally {
+	        DB.closeStatement(st);
+	        DB.closeResultSet(rs);
+	    }
 	}
 
 	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
@@ -226,6 +250,12 @@ public class SellerDaoJDBC implements SellerDao{
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
+	}
+	
+	@Override
+	public void deleteById(Seller id) {
+		
+		
 	}
 	
 	
