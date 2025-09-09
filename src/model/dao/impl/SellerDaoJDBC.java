938f9838e0b79023e15853478d2b68ce57a76189
rhianpb <rhian.pablo16@gmail.com>
2025-09-09 17:14:12 -0300

    delete implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -103,6 +103,7 @@ public class SellerDaoJDBC implements SellerDao{
 			st.setInt(1, id);
 			
 			st.executeUpdate();
+			
 		}
 		catch(SQLException e) {
 			throw new DbException(e.getMessage());
